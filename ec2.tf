db968b2d79c47435356c2a86a9f1b0c3635db19e
Mohammed Khaleel Uddin <151870351+khaleeluddin9912@users.noreply.github.com>
2025-09-11 19:33:00 +0530

    Update ec2.tf
    update the code

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,10 @@
-
+resource "aws_instance" "my_instance" {
+    ami           = "ami-02d26659fd82cf299"  # Ubuntu Linux
+    instance_type = "t2.micro"
+    subnet_id     = aws_subnet.my_private_subnet.id
+    security_groups = [aws_security_group.my_sg.id]
+    tags = {
+        Name = "terra_server"
+    }
+  
+}
