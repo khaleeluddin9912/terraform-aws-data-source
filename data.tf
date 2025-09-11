aa32d60ed6b0e78170e3d84048c90ad2d6a87d57
Mohammed Khaleel Uddin <151870351+khaleeluddin9912@users.noreply.github.com>
2025-09-11 19:48:51 +0530

    Update data.tf
    added code

--- data.tf
+++ data.tf
@@ -1,1 +1,37 @@
 
+# generating ami id 
+data "aws_ami" "name" {
+  most_recent = true
+  owners      = ["amazon"]
+}
+
+# checking the Avilability-Zones 
+data "aws_availability_zones" "names" {
+  state = "available"
+}
+
+#To get the account details
+data "aws_caller_identity" "name" {
+}
+data "aws_region" "name" {
+}
+
+
+#Getting the Security group details and id
+data "aws_security_group" "name" {
+  tags = {
+    name = "MySG"
+    ENV  = "PROD"
+  }
+}
+
+#Getting VPC Id
+data "aws_vpc" "name" {
+  tags = {
+    ENV  = "PROD"
+    Name = "my-vpc"
+  }
+}
+
+
+
