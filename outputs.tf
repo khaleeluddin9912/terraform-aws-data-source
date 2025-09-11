72d1db98c3ec055d03f55dfc2951c167baaf8458
Mohammed Khaleel Uddin <151870351+khaleeluddin9912@users.noreply.github.com>
2025-09-11 19:54:42 +0530

    Update outputs.tf
    add the code

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,23 @@
+output "aws_ami" {
+  value = data.aws_ami.name.id
+}
 
+output "aws_zones" {
+  value = data.aws_availability_zones.names
+}
+
+output "caller_info" {
+  value = data.aws_caller_identity.name
+}
+
+output "security_group" {
+  value = data.aws_security_group.name.id
+}
+
+output "vpc_id" {
+  value = data.aws_vpc.name.id
+}
+
+output "region_name" {
+  value = data.aws_region.name
+}
